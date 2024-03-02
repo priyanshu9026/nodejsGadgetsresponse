acde8cba01ab7a92bf6bf913aea5612de9a40bcc
priyanshu9026 <singpriyasnshu22@gmail.com>
2024-03-02 07:13:22 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// const express = require('express')
+// const app = express()
+
+// app.get('/gadgets', (request, response) => {
+//   response.sendFile('./gadgets.html', {root: __dirname})
+// })
+
+// module.exports = app
+
+const express = require('express')
+
+const app = express()
+
+app.get('/gadgets', (request, response) => {
+  response.sendFile('./gadgets.html', {root: __dirname})
+})
+
+module.exports = app
